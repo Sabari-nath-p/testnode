4cd075183a443576f28ff6efe1cef12164ee11b7
Sabarinath P <98906581+Sabari-nath-p@users.noreply.github.com>
2023-05-30 23:39:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('key.json');
+const serviceAccount = require('./key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
