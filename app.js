dc470abd6499807b4d1ac226ced13647553bccbd
Sabarinath P <98906581+Sabari-nath-p@users.noreply.github.com>
2023-05-30 23:37:20 +0530

    firebase connected

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('key.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Get a reference to the Firestore database
+const db = admin.firestore();
 
 // Define your API routes and handlers
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/', async (req, res) => {
+  try {
+    // Query the Firestore database
+    const snapshot = await db.collection('contents').get();
+    const data = snapshot.docs.map(doc => doc.data());
+
+    res.json(data);
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Start the server
