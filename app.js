c213bf13c166cf45baad3cf235893628098c1dad
Sabarinath P <98906581+Sabari-nath-p@users.noreply.github.com>
2023-05-31 12:45:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/upload/:collection/:documentId', async (req, res) => {
 
     // Add the data to the Firestore collection with the specified document ID
     const documentRef = db.collection(collection).doc(documentId);
-    await documentRef.set(data);
+    await documentRef.set(JSON.stringify(data));
 
     res.json({ message: 'sucess' });
   } catch (error) {
